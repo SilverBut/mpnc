9274ca45d571042469c4fb3cee09f0b38c53afb5
SilverBut <SilverBut@users.noreply.github.com>
2022-02-05 14:01:05 +0800

    add demo server and client

---
+++ src/client.rs
@@ -0,0 +1,38 @@
+use std::fmt::format;
+use std::io::{Read, Write};
+use std::net::TcpStream;
+use std::str::from_utf8;
+
+pub fn client(addr: &str) {
+    let s = format!("{}:3333", addr);
+    println!("{}", s);
+    match TcpStream::connect(s) {
+        Ok(mut stream) => {
+            println!("Successfully connected to server in port 3333");
+
+            let msg = b"Hello!";
+
+            stream.write(msg).unwrap();
+            println!("Sent Hello, awaiting reply...");
+
+            let mut data = [0 as u8; 6]; // using 6 byte buffer
+            match stream.read_exact(&mut data) {
+                Ok(_) => {
+                    if &data == msg {
+                        println!("Reply is ok!");
+                    } else {
+                        let text = from_utf8(&data).unwrap();
+                        println!("Unexpected reply: {}", text);
+                    }
+                }
+                Err(e) => {
+                    println!("Failed to receive data: {}", e);
+                }
+            }
+        }
+        Err(e) => {
+            println!("Failed to connect: {}", e);
+        }
+    }
+    println!("Terminated.");
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+mod client;
+mod server;
+
 fn main() {
     println!("Hello, world!");
+
+    match std::env::args().nth(1).expect("no mode given").as_str() {
+        "server" => server::server(),
+        "client" => client::client(std::env::args().nth(2).expect("no ip given").as_str()),
+        _ => panic!("server or client?"),
+    }
 }
